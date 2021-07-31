a2710c66d22c5d98912d00fe40d778190333cab1
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2021-07-31 10:48:41 -0500

    Agregar archivos de proyecto.

---
+++ Primer proyecto de formularios en Visual C Sharp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Primer_proyecto_de_formularios_en_Visual_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclickthis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnclickthis
+            // 
+            this.btnclickthis.Location = new System.Drawing.Point(340, 391);
+            this.btnclickthis.Name = "btnclickthis";
+            this.btnclickthis.Size = new System.Drawing.Size(75, 23);
+            this.btnclickthis.TabIndex = 0;
+            this.btnclickthis.Text = "click this";
+            this.btnclickthis.UseVisualStyleBackColor = true;
+            this.btnclickthis.Click += new System.EventHandler(this.btnclickthis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnclickthis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclickthis;
+    }
+}
+
---
+++ Primer proyecto de formularios en Visual C Sharp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primer_proyecto_de_formularios_en_Visual_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclickthis_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gerson, Kevin, Genesis, Alejandro, Fernado, Eliseo");
+        }
+    }
+}
